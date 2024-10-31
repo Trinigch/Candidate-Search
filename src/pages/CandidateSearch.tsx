1caa406367de066d738f64406380ba696a653dd6
Trinigch <trinigch@gmail.com>
2024-10-30 20:43:01 -0500

    adding the funcions Candidatesearch nd save candidate

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,67 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
 
-const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
-};
+  const CandidateSearch = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [currentCandidate, setCurrentCandidate] = useState <Candidate | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [loading, setLoading]= useState (false);
 
+    useEffect(() => {
+    const fetchCandidates = async () => {
+      setLoading(true);
+      const data = await searchGithub();
+      setCandidates(data);
+      setLoading(false);
+      }
+      fetchCandidates();
+    }, []);
+
+    useEffect(()=> {
+      if (candidates.length > 0 && currentIndex < candidates.length){
+
+      const fetchCandidateDetails = async () => {
+              setLoading (true);
+              const candidateData = await searchGithubUser(candidates[currentIndex].login);
+              setCurrentCandidate(candidateData);
+              setLoading(false);
+            }
+      fetchCandidateDetails();
+    }
+    }, [currentIndex, candidates]);
+
+  const SaveCandidate = () => {
+    if (currentCandidate) {
+     const savedCandidates = JSON.parse(localStorage.getItem('savedCandidates')|| '[]');
+     savedCandidates.push(currentCandidate);
+     localStorage.setItem('savedCandidates', JSON.stringify(savedCandidates));
+    }
+    showNextCandidate();
+  };
+  console.log("loading", loading);
+const showNextCandidate = () => {
+  setCurrentIndex((prevIndex)=> (prevIndex+1) % candidates.length);
+  };
+   
+  return(
+    <div className='container'>
+      <h1>Candidate Search</h1>
+        {loading ? (
+        <p>Loading candidate details...</p>
+      ) : currentCandidate ? (
+        <div className="card">
+          <div className="card-content">
+            <img src={currentCandidate.avatar_url} alt={`${currentCandidate.login}'s avatar`} />
+            <p><strong>Name:</strong> {currentCandidate.name || currentCandidate.login}</p>
+          </div>
+        </div>
+      ) : (
+        <p>No more candidates available</p>
+      )}
+      <button onClick={SaveCandidate}>Save Candidate</button>
+      <button onClick={showNextCandidate}>Next Candidate</button>
+    </div>
+    );
+  }
 export default CandidateSearch;
--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,7 +1,46 @@
+import {useEffect, useState}  from 'react';
+
+import {Candidate} from '../interfaces/Candidate.interface';
+
+
+
+
 const SavedCandidates = () => {
+  const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const candidates = JSON.parse(localStorage.getItem('savedCandidates')|| '[]');
+    setSavedCandidates(candidates);
+  }, []);
+
+  if (savedCandidates.length===0){
+    return<p> No candidates yet</p>
+  }
   return (
     <>
       <h1>Potential Candidates</h1>
+      <table className="table">
+          <thead>
+            <tr>
+              <th>Avatar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {savedCandidates.map((candidate)=> (
+              <tr key={candidate.login}>
+                    <td>
+                      <img src={candidate.avatar_url} width={50}/>
+                    </td>
+                    <td>{candidate.name||'N/A'}</td>
+                    <td>
+                      <a href={candidate.html_url} target="_blank" rel="non">
+                        Github Profile
+                      </a>
+                    </td>
+              </tr>
+            ))}
+          </tbody>
+      </table>
     </>
   );
 };
