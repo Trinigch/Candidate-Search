0e96b9cd3c00e5c068635d6233a69f655ea9726c
Trinigch <trinigch@gmail.com>
2024-10-30 20:44:33 -0500

    adding interfaces

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    login: string;
+    avatar_url: string;
+    html_url: string;
+    id: number;
+    name?:string | null;
+    email?:string| null;
+    company?:string |null;
+    location?:string|null;
+
+}
+
